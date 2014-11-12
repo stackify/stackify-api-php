1b8d50c65db156370c5f3d32bbdaee83fda41168
Dmitry Tarasov <dimitry@blackopsdev.com>
2014-11-12 16:53:26 +0300

    Replace environment variables by server variables in run scripts

--- run/log4php.php
+++ run/log4php.php
@@ -7,15 +7,11 @@ $logger = Logger::getLogger('logger_name');
 
 session_start();
 $_SESSION['user_id'] = 42;
-$_ENV['a'] = array('a');
-$_ENV['b'] = null;
-$_ENV['c'] = 123;
-$_ENV['d'] = function() {};
-$_ENV['e'] = fopen('D:/work/Stackify/make.sh', 'r');
 
 try {
     include 'exception.php';
 } catch (Exception $ex) {
     $logger->warn('test', $ex);
 }
+
 $logger->debug('foo');
--- run/monolog.php
+++ run/monolog.php
@@ -10,11 +10,6 @@ $logger->pushHandler($handler);
 
 session_start();
 $_SESSION['user_id'] = 42;
-$_ENV['a'] = array('a');
-$_ENV['b'] = null;
-$_ENV['c'] = 123;
-$_ENV['d'] = function() {};
-$_ENV['e'] = fopen('D:/work/Stackify/make.sh', 'r');
 
 try {
     include 'exception.php';
