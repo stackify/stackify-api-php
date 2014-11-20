e3d686c66cf93ebf59beb6f5a9b75e072e073829
Dmitry Tarasov <dimitry@blackopsdev.com>
2014-11-20 12:58:19 +0300

    Set final configuration for agent transport

--- src/Stackify/Log/Transport/Config/Agent.php
+++ src/Stackify/Log/Transport/Config/Agent.php
@@ -2,13 +2,12 @@
 
 namespace Stackify\Log\Transport\Config;
 
-// @TODO set release values
 class Agent
 {
     const SOCKET_PROTOCOL = 'tcp';
     const SOCKET_HOST = '127.0.0.1';
-    const SOCKET_PORT = 1234;
-    const SOCKET_TIMEOUT_CONNECT = 2;
-    const SOCKET_TIMEOUT_WRITE = 2;
+    const SOCKET_PORT = 10515;
+    const SOCKET_TIMEOUT_CONNECT = 1;
+    const SOCKET_TIMEOUT_WRITE = 1;
     const SOCKET_MAX_CONNECT_ATTEMPTS = 2;
 }
