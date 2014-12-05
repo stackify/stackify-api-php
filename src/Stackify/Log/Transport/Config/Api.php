d287d17ff8d9c16d66ce40f2478d9ac2503272fc
Dmitry Tarasov <Dmitry Tarasov>
2014-12-05 16:59:43 +0300

    Set production API URL

--- src/Stackify/Log/Transport/Config/Api.php
+++ src/Stackify/Log/Transport/Config/Api.php
@@ -2,10 +2,9 @@
 
 namespace Stackify\Log\Transport\Config;
 
-// @TODO set release values
 class Api
 {
-    const API_BASE_URL = 'https://dev.stackify.com/API';
+    const API_BASE_URL = 'https://api.stackify.com';
     const API_CALL_LOGS = '/Log/Save';
     const API_MAX_TIME = 5;
     const API_VERSION_HEADER = 'V1';
