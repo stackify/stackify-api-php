c98c005ae91e4177397776b994ad3b59f2692c65
Michael Mantos <mmantos@stackify.com>
2021-04-15 19:26:03 +0800

    Update http socket client signature

--- src/Stackify/Log/Transport/AgentSocketTransport.php
+++ src/Stackify/Log/Transport/AgentSocketTransport.php
@@ -6,6 +6,7 @@ use Http\Client\Exception as HttpClientException;
 use Http\Client\Socket\Client;
 use Http\Message\MessageFactory\GuzzleMessageFactory;
 use Stackify\Log\Transport\Config\Agent as Config;
+use Http\Client\Socket\Exception\ConnectionException as HttpClientSocketConnectionException;
 
 /**
  * This transport requires Stackify Agent to be installed and running on the same machine
@@ -17,35 +18,63 @@ class AgentSocketTransport extends AbstractApiTransport
     const ERROR_WRITE = 'Cannot write to domain socket at %s. Is Stackify Agent installed and running?';
     const ERROR_CONNECT = 'Cannot connect to domain socket at %s. Is Stackify Agent installed and running? [Error code: %d] [Error message: %s]';
 
+    /**
+     * Constructor
+     *
+     * @param array $options Socket transport options
+     */
     public function __construct(array $options = array())
     {
         parent::__construct('NONE', $options);
     }
 
+    /**
+     * Transport name
+     *
+     * @return string
+     */
     protected function getTransportName()
     {
         return 'AgentSocketTransport';
     }
 
+    /**
+     * Flush data to socket
+     *
+     * @param mixed $data Log message
+     * 
+     * @return void
+     */
     protected function send($data)
     {
         try {
             if (!empty($data)) {
                 $messageFactory = new GuzzleMessageFactory();
-                $client = new Client($messageFactory, array('remote_socket' => 'unix://' . $this->getDomainSocketPath()));
+                $client = new Client($messageFactory, null, array('remote_socket' => 'unix://' . $this->getDomainSocketPath()));
 
-                $request = $messageFactory->createRequest('POST', 'http://log',
-                    array('Content-Type' => 'application/json', 'Content-Length' => strlen($data)),
-                    $data);
+                $request = $messageFactory->createRequest(
+                    'POST',
+                    'http://log',
+                    array(
+                        'Content-Type' => 'application/json',
+                        'Content-Length' => strlen($data)
+                    ),
+                    $data
+                );
 
                 $response = $client->sendRequest($request);
 
                 if ($response->getStatusCode() != 200) {
-                    $this->logError(self::ERROR_WRITE, $this->getDomainSocketPath());
+                    $this->logError(
+                        self::ERROR_WRITE,
+                        $this->getDomainSocketPath()
+                    );
                 }
             }
 
         } catch (HttpClientException $e) {
+            $this->logError(self::ERROR_CONNECT, $this->getDomainSocketPath(), $e->getCode(), $e->getMessage());
+        }  catch (HttpClientSocketConnectionException $e) {
             $this->logError(self::ERROR_CONNECT, $this->getDomainSocketPath(), $e->getCode(), $e->getMessage());
         }
     }
